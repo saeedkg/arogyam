8e4965c1c5df62ea86d3cbe0d6ae586d2f64808d
saeedkg <saeedkg586@gmail.com>
2025-10-06 08:13:07 -0700

    search updated

--- lib/landing/ui/components/dashboard_serach_view.dart
+++ lib/landing/ui/components/dashboard_serach_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 
 import '../pages/search_doctors_screen.dart';
 
@@ -11,30 +10,63 @@ class SearchSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      onTap: () {
-        // Navigate to SearchDoctorsScreen using GetX
-        Get.to(() => SearchDoctorsScreen());
-      },
-      decoration: InputDecoration(
-        hintText: 'Search doctors or specialities',
-        hintStyle: TextStyle(color: Colors.grey.shade400),
-        prefixIcon: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: SvgPicture.asset(
-            'assets/icon_svg/ic_search.svg',
-            width: 16,
-            height: 16,
-            color: Colors.grey.shade400,
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            blurRadius: 8,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: TextField(
+        onTap: () {
+          Get.to(() => SearchDoctorsScreen());
+        },
+        readOnly: true, // Makes it clear it's clickable to open search screen
+        decoration: InputDecoration(
+          hintText: 'Search doctors, specialities, or symptoms...',
+          hintStyle: TextStyle(
+            color: Colors.grey.shade500,
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+          ),
+          prefixIcon: Container(
+            margin: const EdgeInsets.all(12),
+            child: SvgPicture.asset(
+              'assets/icon_svg/ic_search.svg',
+              width: 20,
+              height: 20,
+              color: Colors.grey.shade500,
+            ),
+          ),
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: BorderSide.none,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: BorderSide.none,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: const BorderSide(
+              color: Color(0xFF22C58B),
+              width: 1.5,
+            ),
           ),
         ),
-        filled: true,
-        fillColor: Colors.grey.shade100,
-        contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 14),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(16),
-          borderSide: BorderSide.none,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w500,
+          color: Colors.black87,
         ),
+        cursorColor: const Color(0xFF22C58B),
       ),
     );
   }
