9b3affdd673026378e71b1a1cba94911bf88a16d
saeedkgcov <saeed.kg@comave.com>
2025-10-30 12:53:38 +0530

    doctor usl updated

--- lib/find_doctor/constants/doctor_urls.dart
+++ lib/find_doctor/constants/doctor_urls.dart
@@ -3,7 +3,8 @@ import '../../_shared/constants/network_config.dart';
 class DoctorUrls {
   static String getDoctorsListUrl() {
     // Update this path if your backend differs
-    return '${NetworkConfig.baseUrl}/patient/doctors/instant-available';
+    //return '${NetworkConfig.baseUrl}/patient/doctors/instant-available';
+    return '${NetworkConfig.baseUrl}/patient/doctors/featured';
   }
 
   static String getDoctorDetailUrl(String id) {
